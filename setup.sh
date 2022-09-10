5c68510fa6cdb77f9bda86f68455e191c7806a79
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-09 22:53:44 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ cd .config
 mkdir nvim
 cd nvim
 nvim init.vim
+
+sudo apt install curl -y
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
