78ec5c23eb2f03bbf6bcaf69856334862cd4e7db
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-10 16:59:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ cd nvim
 nvim init.vim
 
 sudo apt install curl -y
-curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo apt install python3-pip -y
 sudo pip3 install pywal
