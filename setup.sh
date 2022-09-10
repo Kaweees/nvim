f26d47853aef5fa2845be3190e759ffb0cf98166
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-09 23:08:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,7 @@ nvim init.vim
 sudo apt install curl -y
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo apt install python3-pip -y
+sudo pip3 install pywal
+pywal --theme base16-nord
