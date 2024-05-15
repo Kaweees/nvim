f6faa4d3189153b27f1708048d791cd20c2f3837
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-05-15 12:41:10 -0700

    Clean up config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,19 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+require('config.options')
+
+-- [[ Basic Keymaps ]]
+require('config.keymaps')
+
+-- [[ Configure and install plugins via `packer.nvim` ]]
 require("config.packer")
-require("config.remap")
-require("config.set")
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
